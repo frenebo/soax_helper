b1788696bd713d3c4a1cce84fe331efb1d526a0d
Paul Kreymborg <frenebo@gmail.com>
2021-06-29 11:07:12 -0700

    changed frame rate

--- make_video.py
+++ make_video.py
@@ -6,13 +6,13 @@ if __name__ == "__main__":
     print("usage: arguments are source image folder and target video name something.avi")
     image_folder = sys.argv[1]
     video_name = sys.argv[2]
-    dir_contents = os.listdir(image_folder) 
-    dir_contents.sort()    
+    dir_contents = os.listdir(image_folder)
+    dir_contents.sort()
     images = [img for img in dir_contents if img.endswith(".png")]
     frame = cv2.imread(os.path.join(image_folder, images[0]))
     height, width, layers = frame.shape
 
-    video = cv2.VideoWriter(video_name, 0, 1, (width,height))
+    video = cv2.VideoWriter(video_name, 0, 10, (width,height))
 
     for image in images:
         video.write(cv2.imread(os.path.join(image_folder, image)))
