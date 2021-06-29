2d1e144d6202b5f0debf4c71c7c7055674bf095f
Paul Kreymborg <frenebo@gmail.com>
2021-06-29 10:36:46 -0700

    changed to random col

--- show_snakes.py
+++ show_snakes.py
@@ -13,13 +13,13 @@ def extract_snakes(snake_file,filename):
     snake_dict = {}
     done = False
     lines = [line.strip().split() for line in snake_file]
-    
+
     line_idx = 0
     snake_name = None
     snake_points = None
     while True:
         # if snake name is none, last iteration just finished a snake
-        if snake_name is None:            
+        if snake_name is None:
             line = lines[line_idx]
             #reached junction point section
             if len(line) == 3:
@@ -55,14 +55,14 @@ def extract_snakes(snake_file,filename):
 
             line_idx += 1
             continue
-        
+
     return snake_dict
-        
+
 
 
 if __name__ == "__main__":
     print("usage: 2 arguments: directory with snake text files, directory to save images")
-    
+
     dir_name = sys.argv[1]
     image_dir_name = sys.argv[2]
     filenames = os.listdir(dir_name)
@@ -83,8 +83,9 @@ if __name__ == "__main__":
                 x,y = snake_pts.T
 ##                print(x.shape)
 ##                print(y.shape)
-                plt.plot(x,y,'b')
-            
+                # plt.plot(x,y,'b')
+                plt.plot(x,y)
+
             # some_snakefile.tif => some_snakefile.jpg
             save_img_filename = "".join(filename.split(".")[:-1]) + ".png"
             save_img_fp = os.path.join(image_dir_name,save_img_filename)
