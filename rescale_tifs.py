2a40ef0fbf3eccb15d2d892eeee3feae58a8677c
Paul Kreymborg <frenebo@gmail.com>
2021-07-12 16:04:51 -0700

    changed rescale to work with 3d

--- rescale_tifs.py
+++ rescale_tifs.py
@@ -5,16 +5,47 @@ import matplotlib.pyplot as plt
 import imageio
 import cv2
 from PIL import Image
+from scipy.ndimage import zoom
+import tifffile
+
+def rescale_multi_dim_arr(arr,rescale_factor):
+    if len(arr.shape) > 3:
+        raise Exception("Can't resize array with more than three dimensions")
+    if len(arr.shape) == 3:
+        depth = arr.shape[2]
+    else:
+        depth = None
+
+
+    dims = arr.shape[:2]
+    new_dims = []
+
+    for dim in dims:
+        new_dim = int(dim * rescale_factor)
+        if new_dim == 0:
+            raise Exception("Dimension {} in {} rescaled by factor {} becomes zero".format(dim,fp,args.rescale_factor))
+        new_dims.append(new_dim)
+
+    if depth is not None:
+        print("Resizing {}x{}, depth {} to {}x{}, depth {}".format(dims[0],dims[1],depth,new_dims[0],new_dims[1],depth))
+        new_arr = np.zeros((new_dims[0],new_dims[1],depth),dtype=arr.dtype)
+        for i in range(depth):
+            new_arr[:,:,i] = cv2.resize(arr[i],dsize=new_dims)
+    else:
+        print("Resizing {}x{} to {}x{}".format(dims[0],dims[1],new_dims[0],new_dims[1]))
+        new_arr = cv2.resize(arr,dsize=new_dims)
+
+    return new_arr
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Try some parameters for snakes')
-    parser.add_argument('rescale_factor',type=float,help="Scale factor, for example 0.5 to make tifs half scale")
+    parser.add_argument('rescale_factor',help="Scale factor, for example 0.5 to make tifs half scale")
     parser.add_argument('source_dir',type=readable_dir,help="Directory where source tif files are")
     parser.add_argument('target_dir',type=readable_dir)
 
     args = parser.parse_args()
 
-    if args.rescale_factor <= 0:
+    if float(args.rescale_factor) <= 0:
         raise Exception("Rescale factor must be positive")
 
     tif_files = [filename for filename in os.listdir(args.source_dir) if filename.endswith(".tif")]
@@ -23,24 +54,23 @@ if __name__ == "__main__":
     for filename in tif_files:
         fp = os.path.join(args.source_dir,filename)
 
-        if getattr(Image.open(fp), "n_frames", 1) != 1:
-            raise Exception("Cannot rescale 3d TIF {}".format(fp))
+        pil_img = Image.open(fp)
 
-        img = imageio.imread(fp)
+        # 3D tif images have attribute n_frames with non-zero value
+        img_is_3d = getattr(pil_img, "n_frames", 1) != 1
 
-        dims = img.shape
-        new_dims = []
+        if img_is_3d:
+            print("Resizing {} as 3d image since it has n_frames={}".format(pil_img.n_frames))
+            arr = np.zeros((pil_img.height,pil_img.width,pil_img.n_frames),dtype=np.array(pil_img).dtype)
+            for frame_idx in range(pil_img.n_frames):
+                pil_img.seek(frame_idx)
+                arr[:,:,frame_idx] = np.array(pil_img)
+        else:
+            arr = np.array(pil_img)
 
-        for dim in dims:
-            new_dim = int(dim * args.rescale_factor)
-            if new_dim == 0:
-                raise Exception("Dimension {} in {} rescaled by factor {} becomes zero".format(dim,fp,args.rescale_factor))
-            new_dims.append(new_dim)
+        resized_img = rescale_multi_dim_arr(arr,float(args.rescale_factor))
 
-        new_dims = tuple(new_dims)
+        new_fp = os.path.join(args.target_dir, "resized_{}_".format(args.rescale_factor) + filename)
 
-        resized_img = cv2.resize(img, dsize=new_dims)
-        new_fp = os.path.join(args.target_dir, "resized_" + filename)
-
-        imageio.imwrite(new_fp, resized_img)
+        tifffile.imsave(new_fp, resized_img)
 
