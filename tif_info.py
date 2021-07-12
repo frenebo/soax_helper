cb1632e9f2fe8d81209c22ebf00ddfc26921e37e
Paul Kreymborg <frenebo@gmail.com>
2021-07-12 15:08:38 -0700

    fixed attr name

--- tif_info.py
+++ tif_info.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
 
     for tif_path in tif_paths:
         data = Image.open(tif_path)
-        shape = data.shape
+        shape = data.size
         stack_height = data.n_frames
         arr = np.array(data)
         dtype = str(arr.dtype)
