06bb314572a8b8d2eba8e254077fd88531722847
Paul Kreymborg <frenebo@gmail.com>
2021-07-13 16:04:34 -0700

    added subdir stuff

--- make_video.py
+++ make_video.py
@@ -4,16 +4,7 @@ import os
 import argparse
 from snakeutils.files import readable_dir
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Try some parameters for snakes')
-    parser.add_argument('image_dir',type=readable_dir,help="Source directory to find graphed snakes")
-    parser.add_argument('video_path',help="Path to save video")
-    parser.add_argument('-c','--colorful', action='store_true',help="Use different colors for each snake")
-
-    args = parser.parse_args()
-
-    image_folder = args.image_dir
-    video_path = args.video_path
+def make_and_write_vid(image_folder,video_path):
     dir_contents = os.listdir(image_folder)
     dir_contents.sort()
     images = [img for img in dir_contents if (img.endswith(".png") or img.endswith(".tif"))]
@@ -27,3 +18,27 @@ if __name__ == "__main__":
 
     cv2.destroyAllWindows()
     video.release()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Try some parameters for snakes')
+    parser.add_argument('image_dir',type=readable_dir,help="Source directory to find graphed snakes")
+    parser.add_argument('video_dir',help="Directory to save video(s)")
+    parser.add_argument('--subdirs', default=False, action='store_true',help='If we should make snakes for subdirectories in snake_dir and output in subdirectories in image_dir')
+
+    args = parser.parse_args()
+
+    image_folder = args.image_dir
+    video_dir = args.video_dir
+
+    if args.subdirs:
+        subdir_names = [name for name in os.listdir(dir_name) if os.path.isdir(os.path.join(dir_name,name))]
+
+        print("Making videos for image subdirectories {}".format(subdir_names))
+
+        for subdir_name in subdir_names:
+            subdir_path = os.path.join(image_folder)
+            video_path = os.path.join(video_dir,subdir_name + ".avi")
+            make_and_write_vid(subdir_path,video_path)
+    else:
+        video_path = os.path.join(video_dir, "results.avi")
+        make_and_write_vid(image_folder,video_path)
--- show_snakes.py
+++ show_snakes.py
@@ -5,22 +5,8 @@ import numpy as np
 from snakeutils.files import extract_snakes, readable_dir
 import argparse
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Try some parameters for snakes')
-    parser.add_argument('width',type=int,help="Width dimension of frame")
-    parser.add_argument('height',type=int,help="Width dimension of frame")
-    parser.add_argument('snake_dir',type=readable_dir,help="Source directory where snake text files are")
-    parser.add_argument('image_dir',type=readable_dir,help="Target directory to save graphed snakes")
-    parser.add_argument('-c','--colorful', action='store_true',help="Use different colors for each snake")
-
-    args = parser.parse_args()
-
-    dir_name = args.snake_dir
-    image_dir_name = args.image_dir
-    colorful = args.colorful
-
+def make_snake_images_and_save(dir_name,image_dir_name,image_width,image_height,colorful):
     filenames = os.listdir(dir_name)
-
     for filename in filenames:
         fp = os.path.join(dir_name,filename)
 
@@ -38,14 +24,45 @@ if __name__ == "__main__":
 
             # some_snakefile.tif => some_snakefile.jpg
             save_img_filename = "".join(filename.split(".")[:-1]) + ".png"
-            save_img_fp = os.path.join(image_dir_name,save_img_filename)
+            save_img_fp = os.path.join(image_dir,save_img_filename)
 
             plt.axes().set_aspect('equal', adjustable='box')
             # invert y axis
-            plt.axis([0,args.width,args.height,0])
+            plt.axis([0,image_width,image_height,0])
             plt.xlabel("x")
             plt.ylabel("y")
             plt.savefig(save_img_fp)
             # clear figure so we can do the next plot
             plt.clf()
 
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Try some parameters for snakes')
+    parser.add_argument('width',type=int,help="Width dimension of frame")
+    parser.add_argument('height',type=int,help="Width dimension of frame")
+    parser.add_argument('--subdirs', default=False, action='store_true',help='If we should make snakes for subdirectories in snake_dir and output in subdirectories in image_dir')
+    parser.add_argument('snake_dir',type=readable_dir,help="Source directory where snake text files are")
+    parser.add_argument('image_dir',type=readable_dir,help="Target directory to save graphed snakes")
+    parser.add_argument('-c','--colorful', action='store_true',help="Use different colors for each snake")
+
+    args = parser.parse_args()
+
+    dir_name = args.snake_dir
+    image_dir = args.image_dir
+    image_width = args.width
+    image_height = args.height
+    colorful = args.colorful
+
+    if args.subdirs:
+        subdir_names = [name for name in os.listdir(dir_name) if os.path.isdir(os.path.join(dir_name,name))]
+
+        print("Making snakes for snake subdirectories {}".format(subdir_names))
+
+        for subdir_name in subdir_names:
+            snake_subdir_path = os.path.join(dir_name,subdir_name)
+            image_subdir_path = os.path.join(image_dir,subdir_name)
+            os.mkdir(image_subdir_path)
+            make_snake_images_and_save(snake_subdir_path,image_subdir_path,image_width,image_height,colorful)
+    else:
+        make_snake_images_and_save(dir_name,image_dir,image_width,image_height,colorful)
