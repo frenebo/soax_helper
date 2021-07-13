0e575920ba084bfd91ad035bd9f2a76f2d4e67fb
Paul Kreymborg <frenebo@gmail.com>
2021-07-12 23:47:29 -0700

    added missing assign

--- snakeutils/tifimage.py
+++ snakeutils/tifimage.py
@@ -3,10 +3,10 @@ import tifffile
 
 # numpy arr should have (height,width,depth)
 def save_3d_tif(fp,numpy_arr):
-    print("3D tif orig shape: {}".format(numpy_arr.shape))
+    # print("3D tif orig shape: {}".format(numpy_arr.shape))
     # tif takes (depth,height,width)
-    np.swapaxes(numpy_arr,2,1)
-    np.swapaxes(numpy_arr,1,0)
-    print("3D tif save shape: {}".format(numpy_arr.shape))
+    numpy_arr = np.swapaxes(numpy_arr,2,1)
+    numpy_arr = np.swapaxes(numpy_arr,1,0)
+    # print("3D tif save shape: {}".format(numpy_arr.shape))
 
     tifffile.imsave(fp,numpy_arr)
