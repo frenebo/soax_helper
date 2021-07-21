5fcce149baa2d3db2f4eca7dc78fff2c724a08a7
Paul Kreymborg <frenebo@gmail.com>
2021-07-20 19:07:44 -0700

    added print

--- run_soax_with_params.py
+++ run_soax_with_params.py
@@ -36,6 +36,8 @@ def run_soax(soax_args):
         print("ERROR: ")
         print("Failed to run {}. return code {}".format(command,e.returncode))
 
+    print("Finished!")
+
 def generate_run_soax_args(params_dir,params_filename,output_dir,batch_soax,tif_dir,logging_dir):
     param_fp = os.path.join(params_dir,params_filename)
     params_name = params_filename[:-len(".txt")]
