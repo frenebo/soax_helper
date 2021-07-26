fb92e4546fa5f798b681d2e7d1437aae7afe0a3f
Paul Kreymborg <frenebo@gmail.com>
2021-07-26 14:34:15 -0700

    added amkedir

--- pickle_snakes.py
+++ pickle_snakes.py
@@ -9,24 +9,29 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Try some parameters for snakes')
     parser.add_argument('snakes_dir',help="Directory with snake text files")
-    parser.add_argument('pickle_dir',help="Directory to save pickled snakes")
+    parser.add_argument('pickles_dir',help="Directory to save pickled snakes")
 
     args = parser.parse_args()
 
     dir_name = args.snakes_dir
-    pickle_dir = args.pickle_dir
+    pickles_dir = args.pickles_dir
 
     subfolders, text_filepaths = run_fast_scandir(dir_name,[".txt"])
 
     for text_fp in text_filepaths:
         relative_fp = os.path.relpath(text_fp,dir_name)
+        relative_dir = os.path.dirname(relative_fp)
 
         with open(text_fp, 'r') as snake_file:
             snakes = extract_snakes(snake_file)
 
         # remove .txt and add .pickle
         pickle_relative_fp = relative_fp[:-4] + ".pickle"
-        pickle_fp = os.path.join(pickle_dir,pickle_relative_fp)
+        pickle_fp = os.path.join(pickles_dir,pickle_relative_fp)
+        new_pickle_dir = os.path.dirname(pickle_fp)
+
+        if not os.path.exists(new_pickle_dir):
+            os.makedirs(new_pickle_dir)
 
         with open(pickle_fp, 'wb') as handle:
             pickle.dump(snakes, handle, protocol=pickle.HIGHEST_PROTOCOL)
