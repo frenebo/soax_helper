6c46cb9ab0d68a0c0d7e4d191cbf43777fd048ed
Paul Kreymborg <frenebo@gmail.com>
2021-08-03 15:22:50 -0700

    switching coods

--- join_sectioned_snakes.py
+++ join_sectioned_snakes.py
@@ -63,14 +63,14 @@ def join_snake_files_and_save(source_dir, source_filenames, target_json_fp, logg
                 orig_pos = snake_part["pos"]
                 if are_3d:
                     shifted_pos = [
-                        orig_pos[0] + sec_height_lower,
-                        orig_pos[1] + sec_width_lower,
+                        orig_pos[0] + sec_width_lower,
+                        orig_pos[1] + sec_height_lower,
                         orig_pos[2] + sec_depth_lower,
                     ]
                 else:
                     shifted_pos = [
-                        orig_pos[0] + sec_height_lower,
-                        orig_pos[1] + sec_width_lower
+                        orig_pos[0] + sec_width_lower,
+                        orig_pos[1] + sec_height_lower
                     ]
 
                 shifted_snake.append({
@@ -92,7 +92,6 @@ def join_sectioned_snakes(source_json_dir, target_json_dir, source_jsons_depth,l
     # info_strs = "    ".join(["({},{})".format(fol,fol2) for fol, fol2 in source_folder_info])
     # raise Exception("Source dir {}, target dir {}, depth {}, source info {}".format(source_json_dir, target_json_dir, section_folder_depth, info_strs))
     for containing_folder, source_folder_name in source_folder_info:
-
         relative_dir_path = os.path.relpath(containing_folder, source_json_dir)
         target_dir_path = os.path.join(target_json_dir, relative_dir_path)
         if not os.path.isdir(target_dir_path):
