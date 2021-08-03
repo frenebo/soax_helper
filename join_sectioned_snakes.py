cd1c2c9c055da3b25c9fc9d74c879d01c4c15c81
Paul Kreymborg <frenebo@gmail.com>
2021-08-02 17:27:17 -0700

    doing more

--- join_sectioned_snakes.py
+++ join_sectioned_snakes.py
@@ -1,9 +1,17 @@
-from snakeutils.files import find_files_or_folders_at_depth
+from snakeutils.files import find_files_or_folders_at_depth, extract_snakes
 import os
 import json
 from snakeutils.logger import PrintLogger
 
-def join_sectioned_snakes(source_json_dir, target_json_dir, source_jsons_depth=1):
+def join_snake_files_and_save(source_filepaths, target_json_fp, logger):
+    new_snakes = []
+    for snakes_fp in source_filepaths:
+        with open(snake_fp, "r") as f:
+            section_snakes = extract_snakes(f)
+        for snake in section_snakes:
+            raise Exception("UNIMPLEMENTE")
+
+def join_sectioned_snakes(source_json_dir, target_json_dir, source_jsons_depth=1,logger=PrintLogger):
     if subdir_depth < 1:
         raise Exception("Cannot join sectioned snakes if subdir depth is less than 1. Need a subdirectory full of sectioned snake jsons to produce one joined snake json in the target dir.")
     # The folders containing the source json files to be joined are one level less deep
@@ -24,4 +32,4 @@ def join_sectioned_snakes(source_json_dir, target_json_dir, source_jsons_depth=1
         source_json = [fn for fn in source_files]
         source_json.sort()
 
-        raise Exception("Unimplemented")
+        join_snake_files_and_save(source_json, target_json_fp)
