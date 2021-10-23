e617874cd7a13ed80a7d8135b66d2e8dbed21f2c
Paul Kreymborg <frenebo@gmail.com>
2021-10-22 18:46:54 -0700

    making tube piv temporarily not implemented

--- snakeutils/params.py
+++ snakeutils/params.py
@@ -11,25 +11,31 @@ def create_params(
     external_factor=1,
     intensity_scaling=0.0, # Should be equal to one to skip auto intensity scaling
     stretch_factor=0.2,
+    init_z=True,
+    maximum_iterations=10000,
+    change_threshold=0.1,
+    check_period=100,
+    number_of_background_radial_sectors=8,
+    background_z_xy_ratio=1,
     ):
     params = """intensity-scaling	{intensity_scaling}
 gaussian-std	{gaussian_std}
 ridge-threshold	{ridge_threshold}
 maximum-foreground	65535
 minimum-foreground	{min_foreground}
-init-z	true
+init-z	{init_z_str}
 snake-point-spacing	{snake_point_spacing}
 minimum-snake-length	{min_snake_length}
-maximum-iterations	10000
-change-threshold	0.1
-check-period	100
+maximum-iterations	{maximum_iterations}
+change-threshold	{change_threshold}
+check-period	{check_period}
 alpha	{alpha}
 beta	{beta}
 gamma	{gamma}
 external-factor	{external_factor}
 stretch-factor	{stretch_factor}
-number-of-background-radial-sectors	8
-background-z-xy-ratio	2.88
+number-of-background-radial-sectors	{number_of_background_radial_sectors}
+background-z-xy-ratio	{background_z_xy_ratio}
 radial-near	4
 radial-far	8
 delta	4
@@ -49,5 +55,11 @@ damp-z	false""".format(
         external_factor=external_factor,
         intensity_scaling=intensity_scaling,
         stretch_factor=stretch_factor,
+        init_z_str=("true" if init_z else "false"),
+        maximum_iterations=maximum_iterations,
+        change_threshold=change_threshold,
+        check_period=check_period,
+        number_of_background_radial_sectors=number_of_background_radial_sectors,
+        background_z_xy_ratio=background_z_xy_ratio,
         )
     return params
--- tube_piv.py
+++ tube_piv.py
@@ -7,84 +7,84 @@ import os
 from snakeutils.logger import PrintLogger
 from snakeutils.files import find_files_or_folders_at_depth, pil_img_3d_to_np_arr
 
-def loadData(file_path):
-    dataset = Image.open(file_path)
-    h,w = np.shape(dataset)
-    tiffarray = np.zeros((h,w,dataset.n_frames))
-    for i in range(dataset.n_frames):
-        dataset.seek(i)
-        tiffarray[:,:,i] = np.array(dataset)
-    return tiffarray.astype(np.double)
+# def loadData(file_path):
+#     dataset = Image.open(file_path)
+#     h,w = np.shape(dataset)
+#     tiffarray = np.zeros((h,w,dataset.n_frames))
+#     for i in range(dataset.n_frames):
+#         dataset.seek(i)
+#         tiffarray[:,:,i] = np.array(dataset)
+#     return tiffarray.astype(np.double)
 
-def xcorr3fft(image1,image2):
+# def xcorr3fft(image1,image2):
 
-    F = np.fft.fftn(image1);
-    Fc = np.conj(np.fft.fftn(image2));
+#     F = np.fft.fftn(image1);
+#     Fc = np.conj(np.fft.fftn(image2));
 
-    R = F.*Fc; # Phase correlation, Compute fft of image1 and conjugate fft of image2, elementwise multiply and normalise.
-    c = np.fft.ifftn(R); # Inverse fft of Phase correlation gives cross correlation map.
-    [~,i] = np.max(c);
+#     R = F.*Fc; # Phase correlation, Compute fft of image1 and conjugate fft of image2, elementwise multiply and normalise.
+#     c = np.fft.ifftn(R); # Inverse fft of Phase correlation gives cross correlation map.
+#     [~,i] = np.max(c);
 
-    [I,J,K] = np.unravel_index(np.shape(c),i);
+#     [I,J,K] = np.unravel_index(np.shape(c),i);
 
 
-    if abs(I-1)<abs(np.shape(image1)[0]-I+1):
-        shiftx = -I+1;
-    else:
-        shiftx =  np.shape(image1)[0]-I+1;
+#     if abs(I-1)<abs(np.shape(image1)[0]-I+1):
+#         shiftx = -I+1;
+#     else:
+#         shiftx =  np.shape(image1)[0]-I+1;
 
-    if abs(J-1)<abs(np.shape(image1)[1]-J+1):
-        shifty = -J+1;
-    else:
-        shifty = np.shape(image1)[1]-J+1;
+#     if abs(J-1)<abs(np.shape(image1)[1]-J+1):
+#         shifty = -J+1;
+#     else:
+#         shifty = np.shape(image1)[1]-J+1;
 
-    if abs(K-1)<abs(np.shape(image1)[2]-K+1):
-        shiftz = -K+1;
-    else:
-        shiftz = np.shape(image1)[2]-K+1;
+#     if abs(K-1)<abs(np.shape(image1)[2]-K+1):
+#         shiftz = -K+1;
+#     else:
+#         shiftz = np.shape(image1)[2]-K+1;
 
-    return shiftx,shifty,shiftz
+#     return shiftx,shifty,shiftz
 
-def GetPIV3d(im1,im2,X1,Y1,Z1,edge_length):
-    si = np.shape(im1);
-    nb = np.ceil(si/edge_length); #Floor?
-    counter = 0;
-    shiftx = np.zeros((1,nb[1]*nb[2]*nb[3]));
-    shifty = np.zeros_like(shiftx);
-    shiftz = np.zeros_like(shiftx);
-    x = np.zeros_like(shiftx);
-    y = np.zeros_like(shiftx);
-    z = np.zeros_like(shiftx);
-    window = 15;
+# def GetPIV3d(im1,im2,X1,Y1,Z1,edge_length):
+#     si = np.shape(im1);
+#     nb = np.ceil(si/edge_length); #Floor?
+#     counter = 0;
+#     shiftx = np.zeros((1,nb[1]*nb[2]*nb[3]));
+#     shifty = np.zeros_like(shiftx);
+#     shiftz = np.zeros_like(shiftx);
+#     x = np.zeros_like(shiftx);
+#     y = np.zeros_like(shiftx);
+#     z = np.zeros_like(shiftx);
+#     window = 15;
 
-    for i in range(0, nb[1]):
-        for j in range(0, nb[2]):
-            for k in range(0, nb[3]):
+#     for i in range(0, nb[1]):
+#         for j in range(0, nb[2]):
+#             for k in range(0, nb[3]):
 
-                #cut a cube out of the image
-                temp1 = im1[
-                        max(0,((i-1)*edge_length-window)):min((i+1)*edge_length+window,si[0]),
-                        max(0,(j-1)*edge_length-window)):min((j+1)*edge_length+window,si[1]),
-                        max(0,(k-1)*edge_length-window)):min((k+1)*edge_length+window,si[2]),
-                        ]
+#                 #cut a cube out of the image
+#                 temp1 = im1[
+#                         max(0,((i-1)*edge_length-window)):min((i+1)*edge_length+window,si[0]),
+#                         max(0,(j-1)*edge_length-window)):min((j+1)*edge_length+window,si[1]),
+#                         max(0,(k-1)*edge_length-window)):min((k+1)*edge_length+window,si[2]),
+#                         ]
 
-                temp2 = im2[
-                        max(0,((i-1)*edge_length-window)):min((i+1)*edge_length+window,si[0]),
-                        max(0,(j-1)*edge_length-window):min((j+1)*edge_length+window,si[1]),
-                        max(0,(k-1)*edge_length-window):min((k+1)*edge_length+window,si[2]),
-                        ]
+#                 temp2 = im2[
+#                         max(0,((i-1)*edge_length-window)):min((i+1)*edge_length+window,si[0]),
+#                         max(0,(j-1)*edge_length-window):min((j+1)*edge_length+window,si[1]),
+#                         max(0,(k-1)*edge_length-window):min((k+1)*edge_length+window,si[2]),
+#                         ]
 
-                shiftx[counter],shifty[counter],shiftz[counter] = xcorr3fft(temp1,temp2);
-                x[counter] = (i-0.5)*edge_length;
-                y[counter] = (j-0.5)*edge_length;
-                z[counter] = (k-0.5)*edge_length;
-                counter = counter+1;
+#                 shiftx[counter],shifty[counter],shiftz[counter] = xcorr3fft(temp1,temp2);
+#                 x[counter] = (i-0.5)*edge_length;
+#                 y[counter] = (j-0.5)*edge_length;
+#                 z[counter] = (k-0.5)*edge_length;
+#                 counter = counter+1;
 
-    # Put the flow field on a grid
-    VX  = griddata(x,y,z,shiftx,X1,Y1,Z1);
-    VY  = griddata(x,y,z,shifty,X1,Y1,Z1);
-    VZ  = griddata(x,y,z,shiftz,X1,Y1,Z1);
-    return VX,VY,VZ
+#     # Put the flow field on a grid
+#     VX  = griddata(x,y,z,shiftx,X1,Y1,Z1);
+#     VY  = griddata(x,y,z,shifty,X1,Y1,Z1);
+#     VZ  = griddata(x,y,z,shiftz,X1,Y1,Z1);
+#     return VX,VY,VZ
 
 def tube_piv(
     source_tiff_dir,
@@ -92,6 +92,7 @@ def tube_piv(
     edge_length=5,
     logger=PrintLogger,
     ):
+    raise NotImplementedError()
     source_dir_info = find_files_or_folders_at_depth(source_tiff_dir, 0, file_extensions=[".tif", ".tiff"])
 
 
