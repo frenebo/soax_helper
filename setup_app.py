107ad2a01d9233da860b4bc2721f2340273b80e5
Paul Kreymborg <frenebo@gmail.com>
2021-10-22 17:49:58 -0700

    add default menu opt

--- setup_app.py
+++ setup_app.py
@@ -391,7 +391,7 @@ class SetupForm(npyscreen.Form):
         except ParseException as e:
             if isinstance(e, DirectoryDoesNotExistParseException):
                 dirpath = str(e)
-                should_make_dir = npyscreen.notify_yes_no(str("Directory {} does not exist. Create?").format(dirpath))
+                should_make_dir = npyscreen.notify_yes_no(str("Directory {} does not exist. Create?").format(dirpath), editw=1)
                 # Make dirs and try again
                 if should_make_dir:
                     os.makedirs(dirpath)
