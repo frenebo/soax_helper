4d6e328c0299b318d1c732bc719e35b119a209e3
Paul Kreymborg <frenebo@gmail.com>
2021-10-22 18:47:24 -0700

    making tube piv cocmmented out

--- tube_piv.py
+++ tube_piv.py
@@ -93,82 +93,82 @@ def tube_piv(
     logger=PrintLogger,
     ):
     raise NotImplementedError()
-    source_dir_info = find_files_or_folders_at_depth(source_tiff_dir, 0, file_extensions=[".tif", ".tiff"])
+    # source_dir_info = find_files_or_folders_at_depth(source_tiff_dir, 0, file_extensions=[".tif", ".tiff"])
 
 
-    for time_idx in len(source_dir_info - 1):
-        containing_folder1, tif_name1 = source_dir_info[time_idx]
-        im1_path = os.path.join(containing_folder1, tif_name1)
-        containing_folder2, tif_name2 = source_dir_info[time_idx + 1]
-        im2_path = os.path.join(containing_folder2, tif_name2)
-        im1 = loadData(im1_path)
-        im2 = loadData(im2_path)
+    # for time_idx in len(source_dir_info - 1):
+    #     containing_folder1, tif_name1 = source_dir_info[time_idx]
+    #     im1_path = os.path.join(containing_folder1, tif_name1)
+    #     containing_folder2, tif_name2 = source_dir_info[time_idx + 1]
+    #     im2_path = os.path.join(containing_folder2, tif_name2)
+    #     im1 = loadData(im1_path)
+    #     im2 = loadData(im2_path)
 
-        L1, L2, L3 = np.shape(im1)
+    #     L1, L2, L3 = np.shape(im1)
 
-        # define the grid on which to compute the flow field
-        [X1,Y1,Z1] = np.meshgrid(np.linspace(edge_length/2, L1-edge_length/2, int(L1/edge_length-1)),
-                                np.linspace(edge_length/2, L2-edge_length/2, int(L3/edge_length-1)),
-                                np.linspace(edge_length/2, L2-edge_length/2, int(L3/edge_length-1)))
+    #     # define the grid on which to compute the flow field
+    #     [X1,Y1,Z1] = np.meshgrid(np.linspace(edge_length/2, L1-edge_length/2, int(L1/edge_length-1)),
+    #                             np.linspace(edge_length/2, L2-edge_length/2, int(L3/edge_length-1)),
+    #                             np.linspace(edge_length/2, L2-edge_length/2, int(L3/edge_length-1)))
 
-        shiftx, shifty, shiftz = GetPIV3d(im1, im2, X1, Y1, Z1, edge_length)
+    #     shiftx, shifty, shiftz = GetPIV3d(im1, im2, X1, Y1, Z1, edge_length)
 
-        # im1 = pil_img_3d_to_np_arr(im1_path)
+    #     # im1 = pil_img_3d_to_np_arr(im1_path)
 
-    # velfolder='\PIV'; #Open folder
+    # # velfolder='\PIV'; #Open folder
 
-    # if (isfolder(basepath + velfolder)!=1):
-    #     mkdir(basepath + velfolder)
+    # # if (isfolder(basepath + velfolder)!=1):
+    # #     mkdir(basepath + velfolder)
 
-    NTimes = 20;
-    timestart = 10;
-    isf = 1;          # image scaling factor.
-    step = 1;         # step in timeframes.
-    smooth = False
-    sigma = .8;       # standard deviation of gaussian kernel
-    im1 = loadData(basepath + '\TP'+t+'_Ch0_Ill0_Ang0,90,180,270.tif');
-    im1 = cv2.resize(im1, dsize=isf*np.shape(im1),
-                    interpolation=cv2.INTER_CUBIC);
-    stackSize = np.shape(im1)[2];
-    slice_index = np.linspace(0, stackSize);
-    temp_im = loadData(basepath + '\TP'+timestart+'_Ch0_Ill0_Ang0,90,180,270.tif');
-    for sl in range(0, len(slice_index)):
-        tempImSl = temp_im[slice_index[sl]]
-        im1(:,:,sl) = cv2.resize(tempImSl, dsize=isf*np.shape(tempImSl),
-                                interpolation=cv2.INTER_CUBIC);
+    # NTimes = 20;
+    # timestart = 10;
+    # isf = 1;          # image scaling factor.
+    # step = 1;         # step in timeframes.
+    # smooth = False
+    # sigma = .8;       # standard deviation of gaussian kernel
+    # im1 = loadData(basepath + '\TP'+t+'_Ch0_Ill0_Ang0,90,180,270.tif');
+    # im1 = cv2.resize(im1, dsize=isf*np.shape(im1),
+    #                 interpolation=cv2.INTER_CUBIC);
+    # stackSize = np.shape(im1)[2];
+    # slice_index = np.linspace(0, stackSize);
+    # temp_im = loadData(basepath + '\TP'+timestart+'_Ch0_Ill0_Ang0,90,180,270.tif');
+    # for sl in range(0, len(slice_index)):
+    #     tempImSl = temp_im[slice_index[sl]]
+    #     im1(:,:,sl) = cv2.resize(tempImSl, dsize=isf*np.shape(tempImSl),
+    #                             interpolation=cv2.INTER_CUBIC);
 
-    for time in range(timestart, timestart+NTimes):
-        #read the images
-        tp_1_name = basepath + '\TP'+time+'_Ch0_Ill0_Ang0,90,180,270.tif';
-        tp_2_name = basepath + '\TP'+str(time+step)+'_Ch0_Ill0_Ang0,90,180,270.tif';
+    # for time in range(timestart, timestart+NTimes):
+    #     #read the images
+    #     tp_1_name = basepath + '\TP'+time+'_Ch0_Ill0_Ang0,90,180,270.tif';
+    #     tp_2_name = basepath + '\TP'+str(time+step)+'_Ch0_Ill0_Ang0,90,180,270.tif';
 
-        #read Image stacks and down sample
-        if time == 0:
-            stackSize = len(imfinfo(tp_1_name));
+    #     #read Image stacks and down sample
+    #     if time == 0:
+    #         stackSize = len(imfinfo(tp_1_name));
 
-        slice_index = np.linspace(0, stackSize, int(stackSize/isf));
-        temp_im1 = loadData(tp_1_name);
-        temp_im2 = loadData(tp_2_name);
+    #     slice_index = np.linspace(0, stackSize, int(stackSize/isf));
+    #     temp_im1 = loadData(tp_1_name);
+    #     temp_im2 = loadData(tp_2_name);
 
-        for sl in range(0,  len(slice_index)):
-            tempSl1 = temp_im1[slice_index[sl]]
-            im1[:,:,sl] = cv2.resize(tempSl1, dsize=isf*np.shape(tempSl1),
-                                    interpolation=cv2.INTER_CUBIC);
+    #     for sl in range(0,  len(slice_index)):
+    #         tempSl1 = temp_im1[slice_index[sl]]
+    #         im1[:,:,sl] = cv2.resize(tempSl1, dsize=isf*np.shape(tempSl1),
+    #                                 interpolation=cv2.INTER_CUBIC);
 
-            tempSl2 = temp_im2[slice_index[sl]]
-            im2[:,:,sl] = cv2.resize(tempSl2, dsize=isf*np.shape(tempSl2),
-                                    interpolation=cv2.INTER_CUBIC);
+    #         tempSl2 = temp_im2[slice_index[sl]]
+    #         im2[:,:,sl] = cv2.resize(tempSl2, dsize=isf*np.shape(tempSl2),
+    #                                 interpolation=cv2.INTER_CUBIC);
 
-        #compute the piv flow field
-        VY,VX,VZ = GetPIV3d(im1,im2,X1,Y1,Z1,edge_length);
+    #     #compute the piv flow field
+    #     VY,VX,VZ = GetPIV3d(im1,im2,X1,Y1,Z1,edge_length);
 
-        #smooth if desired
-        if (smooth == 1):
-            VX  = imgaussfilt3(VX, sigma);
-            VY  = imgaussfilt3(VY, sigma);
-            VZ  = imgaussfilt3(VZ, sigma);
+    #     #smooth if desired
+    #     if (smooth == 1):
+    #         VX  = imgaussfilt3(VX, sigma);
+    #         VY  = imgaussfilt3(VY, sigma);
+    #         VZ  = imgaussfilt3(VZ, sigma);
 
-        np.save(basepath + folder + '/VX_'+time+'.npy',VX);
-        np.save(basepath + folder + '/VY_'+time+'.npy',VY);
-        np.save(basepath + folder + '/VZ_'+time+'.npy',VZ);
+    #     np.save(basepath + folder + '/VX_'+time+'.npy',VX);
+    #     np.save(basepath + folder + '/VY_'+time+'.npy',VY);
+    #     np.save(basepath + folder + '/VZ_'+time+'.npy',VZ);
 
