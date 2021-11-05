ff9200e9d39aa9aa6fb1b76b4d932e66251706ec
Paul Kreymborg <frenebo@gmail.com>
2021-11-05 16:28:49 -0700

    ix err

--- divide_average_image.py
+++ divide_average_image.py
@@ -46,7 +46,7 @@ def divide_average_image(source_dir, target_dir, logger=PrintLogger):
         pil_img = Image.open(os.path.join(source_dir, tiff_name))
         np_arr = pil_img_3d_to_np_arr(pil_img)
         # original_dtype = np_arr.dtype
-        divided_arr = np.multiply(np_arr.astype(np.double) * image_mult_factor)
+        divided_arr = np.multiply(np_arr.astype(np.double), image_mult_factor)
         divide_arr = divide_arr.astype(np_arr.dtype)
         print(divide_arr.dtype)
         # where_less_than_average = np_arr < average_image
