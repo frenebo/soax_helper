3dc35379c856423e62025b3b925d3c5b8c366d5c
Paul Kreymborg <frenebo@gmail.com>
2021-11-08 00:20:26 -0800

    var names

--- split_stack.py
+++ split_stack.py
@@ -19,10 +19,10 @@ if __name__ == "__main__":
     pil_img = Image.open(args.source_tiff_path)
     np_arr = pil_img_3d_to_np_arr(pil_img)
 
-    tiff_name = os.path.split(source_tiff_path)[-1]
+    tiff_name = os.path.split(args.source_tiff_path)[-1]
 
     stack_num = np_arr.shape[2]
     for i in range(stack_num):
-        fp = os.path.join(target_directory, "{}_".format(i) + tiff_name)
+        fp = os.path.join(args.target_directory, "{}_".format(i) + tiff_name)
         save_3d_tif(fp, save_3d_np_arr[i])
         print("Saved {}".format(fp))
