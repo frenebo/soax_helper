867b860b2cc3bbb0a63bb0c15412f13635e586f1
Paul Kreymborg <frenebo@gmail.com>
2021-12-06 15:10:39 -0800

    fix var names

--- pad_tif_numbers.py
+++ pad_tif_numbers.py
@@ -3,17 +3,17 @@ from snakeutils.logger import PrintLogger
 import os
 from snakeutils.files import has_one_of_extensions
 
-def get_num_of_tif_fn(tiff_fn, tiff_name_prefix, logger):
-    fn_without_extension = os.path.splitext(tif_fn)[0]
+def get_num_of_tiff_fn(tiff_fn, tiff_name_prefix, logger):
+    fn_without_extension = os.path.splitext(tiff_fn)[0]
     if not fn_without_extension.startswith(tiff_name_prefix):
-        logger.error("Tif '{}' does not start with '{}', skipping".format(tif_fn, tiff_name_prefix))
+        logger.error("Tif '{}' does not start with '{}', skipping".format(tiff_fn, tiff_name_prefix))
         return None
-    tif_num_str = fn_without_extension[len(tiff_name_prefix):]
+    tiff_num_str = fn_without_extension[len(tiff_name_prefix):]
     try:
-        tif_num = int(tif_num_str)
+        tiff_num = int(tiff_num_str)
     except ValueError as e:
-        logger.FAIL("Cannot parse string '{}' from '{}' as integer".format(tif_num_str, tif_fn))
-    return tif_num
+        logger.FAIL("Cannot parse string '{}' from '{}' as integer".format(tiff_num_str, tiff_fn))
+    return tiff_num
 
 def pad_tif_numbers(
     tiff_dir,
@@ -25,23 +25,23 @@ def pad_tif_numbers(
 
     most_digits = 0
     for tiff_fn in source_tifs:
-        tif_fn_num = get_num_of_tif_fn(tiff_fn, tiff_name_prefix, logger)
-        if tif_fn_num is None:
+        tiff_fn_num = get_num_of_tiff_fn(tiff_fn, tiff_name_prefix, logger)
+        if tiff_fn_num is None:
             continue
-        num_digits = len(str(tif_fn_num))
+        num_digits = len(str(tiff_fn_num))
         if num_digits > most_digits:
             most_digits = num_digits
 
     # Rename files
     for tiff_fn in source_tifs:
-        tif_fn_num = get_num_of_tif_fn(tiff_fn, tiff_name_prefix, logger)
-        if tif_fn_num is None:
+        tiff_fn_num = get_num_of_tiff_fn(tiff_fn, tiff_name_prefix, logger)
+        if tiff_fn_num is None:
             continue
 
-        new_tiff_fn = tiff_name_prefix + str(tif_fn_num).zfill(most_digits) + ".tifs"
-        old_fp = os.path.join(tiff_dir, tif_fn)
+        new_tiff_fn = tiff_name_prefix + str(tiff_fn_num).zfill(most_digits) + ".tifs"
+        old_fp = os.path.join(tiff_dir, tiff_fn)
         new_fp = os.path.join(tiff_dir, new_tiff_fn)
-        logger.log("Renaming {} to {}".format(tif_fn, new_tiff_fn))
+        logger.log("Renaming {} to {}".format(tiff_fn, new_tiff_fn))
         os.rename(old_fp, new_fp)
 
 
@@ -52,4 +52,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    pad_tif_numbers(args.tiff_dir, args.tiff_name_prefix)
+    pad_tiff_numbers(args.tiff_dir, args.tiff_name_prefix)
