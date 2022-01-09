0bfe9142330e450d7e69b345808517995693106e
Paul Kreymborg <frenebo@gmail.com>
2022-01-08 19:05:17 -0800

    log piping parenthesis message to green text ssuccess message

--- src/soax_helper/run_soax.py
+++ src/soax_helper/run_soax.py
@@ -28,7 +28,8 @@ def soax_instance(soax_instance_args):
             snakes_output_dir=snakes_output_dir,
         )
 
-        logger.log("Executing '{}'\n\t(Piping stdout to '{}' and stderr to '{}')".format(command, stdout_fp, stderr_fp))
+        logger.log("Executing '{}'")
+        logger.success("    (Piping stdout to '{}' and stderr to '{}')".format(command, stdout_fp, stderr_fp))
         try:
             code = subprocess.run(command,shell=True,stdout=stdout_file,stderr=error_file,check=True).returncode
             logger.success("Completed {}".format(command))
