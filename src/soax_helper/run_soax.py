76efba645324e3907236cefa142f8b0f1867b9d4
Paul Kreymborg <frenebo@gmail.com>
2022-01-09 15:32:54 -0800

    stop logging details in green

--- src/soax_helper/run_soax.py
+++ src/soax_helper/run_soax.py
@@ -29,7 +29,7 @@ def soax_instance(soax_instance_args):
         )
 
         logger.log("Executing '{}'".format(command))
-        logger.success("    (Piping stdout to '{}' and stderr to '{}')".format(stdout_fp, stderr_fp))
+        logger.log("    (Piping stdout to '{}' and stderr to '{}')".format(stdout_fp, stderr_fp))
         try:
             code = subprocess.run(command,shell=True,stdout=stdout_file,stderr=error_file,check=True).returncode
             logger.success("Completed {}".format(command))
