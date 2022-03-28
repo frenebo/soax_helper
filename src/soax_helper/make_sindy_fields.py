a5729f7a4bae3c7a2d5dbe569da79f4b8cc5b279
Paul Kreymborg <frenebo@gmail.com>
2022-03-28 15:10:18 -0700

    remove extra logging

--- src/soax_helper/make_sindy_fields.py
+++ src/soax_helper/make_sindy_fields.py
@@ -186,12 +186,12 @@ def make_fields(
         snake_pts = get_json_snake_xyz(snake)
         interval_orientations = orientation_of_snake_segments(snake_pts)
         interval_Qtensors = Qtensor(interval_orientations)
-        if i == 0:
-            logger.log("Interval qtensors shape: {}".format(interval_Qtensors.shape))
-            logger.log("Interval Orientations shape: {}".format(interval_orientations.shape))
-            nx,ny,nz = interval_orientations
-            logger.log("Interval orientation first: {}, {}, {}".format(nx[0],ny[0],nz[0]))
-            logger.log("First Qtensor: {}".format(interval_Qtensors[:,:,0]))
+        # if i == 0:
+        #     logger.log("Interval qtensors shape: {}".format(interval_Qtensors.shape))
+        #     logger.log("Interval Orientations shape: {}".format(interval_orientations.shape))
+        #     nx,ny,nz = interval_orientations
+        #     logger.log("Interval orientation first: {}, {}, {}".format(nx[0],ny[0],nz[0]))
+        #     logger.log("First Qtensor: {}".format(interval_Qtensors[:,:,0]))
 
         for interval_idx in range(snake_pts.shape[1] - 1):
             interval_Q = interval_Qtensors[:,:,interval_idx]
